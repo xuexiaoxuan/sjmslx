d595bd69ef032ba8334b8e804c3bc6455465ff51
薛晓轩 <1075654960@qq.com>
2020-09-25 11:36:04 +0800

    zh

---
+++ 15组合模式/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15组合模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
