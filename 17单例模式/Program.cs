c91969b04a66a7e004cc534d9e54635ab0112bd6
薛晓轩 <1075654960@qq.com>
2020-09-27 16:46:12 +0800

    dl

---
+++ 17单例模式/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17单例模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 17单例模式/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17单例模式
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object syncRoot = new object();
+        private Singleton()
+        {
+
+        }
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
