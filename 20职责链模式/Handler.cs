0a9baac9c9cb175f3da288b179b68410589a6586
薛晓轩 <1075654960@qq.com>
2024-01-13 00:21:18 +0800

    #添加职责链模式

---
+++ 20职责链模式/Handler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20职责链模式
+{
+    public abstract class Handler
+    {
+        protected Handler successor;
+
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+}
---
+++ 20职责链模式/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _20职责链模式;
+
+Handler h1 = new ConcreteHandler1();
+Handler h2 = new ConcreteHandler2();
+Handler h3 = new ConcreteHandler3();
+h1.SetSuccessor(h2);
+h2.SetSuccessor(h3);
+int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+foreach (int i in requests)
+{
+    h1.HandleRequest(i);
+}
+
+Console.WriteLine("Hello, World!");
